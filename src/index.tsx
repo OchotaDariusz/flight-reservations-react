7dda10200d2628d7db8b2fc7dc3fa2d54d270ee3
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-04-15 11:33:04 +0200

    chore: added dummy pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,35 @@ import ReactDOM from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import { About, Contact, ErrorPage, FlightSearch, Home } from './pages';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.scss';
-import { Home } from './components';
 
 const router = createHashRouter([
   {
     path: '/',
     element: <App />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/search',
+        element: <FlightSearch />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
       },
     ],
   },
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { About } from './about/About';
+export { Contact } from './contact/Contact';
+export { ErrorPage } from './error-page/ErrorPage';
+export { FlightSearch } from './flight-search/FlightSearch';
+export { Home } from './home/Home';
