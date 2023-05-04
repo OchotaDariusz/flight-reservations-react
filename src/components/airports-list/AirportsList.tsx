78b96b68fde8a29102d7688eac31da06c70bf825
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-05-04 10:15:28 +0200

    wip: flight search

--- src/components/airports-list/AirportsList.tsx
+++ src/components/airports-list/AirportsList.tsx
@@ -1,4 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Airport', width: 130 },
+  { field: 'city', headerName: 'City', width: 130 },
+  {
+    field: 'iataCode',
+    headerName: 'IATA',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'icaoCode',
+    headerName: 'ICAO',
+    type: 'number',
+    width: 90,
+  },
+];
 
 type Props = {
   countryId: number;
@@ -19,12 +38,29 @@ const AirportsList: React.FC<Props> = ({ countryId }) => {
     }
   }, [countryId]);
 
+  const airports = listOfAirports.map((airport) => ({
+    id: airport.id,
+    name: airport.name,
+    city: airport.city,
+    iataCode: airport.iataCode,
+    icaoCode: airport.icaoCode,
+  }));
+
   return (
     <>
-      <div>Airports</div>
-      {listOfAirports.map((airport) => (
-        <div key={airport.id}>{airport.name}</div>
-      ))}
+      <div style={{ height: '100%', width: '100%', paddingBottom: '2rem' }}>
+        <h2>Airports</h2>
+        <DataGrid
+          rows={airports}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+        />
+      </div>
     </>
   );
 };
